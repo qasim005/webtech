70a8928b5cc60fa3dc0265eb58507985837cf033
qasim amin <qasim2646@gmail.com>
2022-03-14 01:56:13 +0500

    Updates

---
+++ precache-manifest.9d92d6139d9901dcab366ece35d41e25.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac03bcf45fb02e9863435ca7b96cd599",
+    "url": "/webtech/index.html"
+  },
+  {
+    "revision": "1e4cebe24c7625323a01",
+    "url": "/webtech/static/css/main.f9f498d2.chunk.css"
+  },
+  {
+    "revision": "a5ee1c0e49980f3c8bf1",
+    "url": "/webtech/static/js/2.600ef9f5.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/webtech/static/js/2.600ef9f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e4cebe24c7625323a01",
+    "url": "/webtech/static/js/main.0b44f4d8.chunk.js"
+  },
+  {
+    "revision": "c1c764378a1ef2572b23",
+    "url": "/webtech/static/js/runtime-main.d68785c3.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/webtech/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/webtech/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/webtech/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/webtech/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/webtech/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/webtech/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/webtech/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/webtech/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/webtech/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/webtech/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/webtech/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/webtech/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/webtech/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/webtech/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
